c77f5039f3e8ea75ebeb4c541730ad046b8f0470
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2018-02-13 11:06:56 +0200

    Persisting data using Core Data

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dmytro Mykhailov on 1/23/18.
+//  Copyright © 2018 DevRaccoon. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var isDone = false
+}
